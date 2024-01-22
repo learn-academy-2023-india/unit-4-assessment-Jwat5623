e73fff8d041cead6d84d5d01011b229266902ee4
jwat5623 <jamar34watson56@gmail.com>
2024-01-22 04:33:28 -0800

    adds completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -18,9 +18,26 @@ const fibonacciLength1 = 6
 const fibonacciLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+describe("fibonacciLength", () => {
+    it("a function that takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function", () => {
+      const result = fibonacciLength(6)
+      expect(result).toEqual([1, 1, 2, 3, 5, 8])
+    })
+  
+    it("a function that takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function", () => {
+      const result = fibonacciLength(10)
+      expect(result).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+})
 // b) Create the function that makes the test pass.
-
-// Pseudo code:
+const fibonacciLength = (length) => {
+    const sequence = [1, 1]
+    for (let i = 2; i < length; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2])
+    }
+    return sequence
+} 
+// Pseudo code: build a function that uses any number more than to create an array of numbers of that same numbers length in the fibonacci sequence
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
@@ -31,6 +48,21 @@ const votes1 = { upVotes: 13, downVotes: 2 }
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+describe("tallyVotes", () => {
+    it("a function that takes in an object that contains up votes and down votes and returns the end tally for votes1", () => {
+        const result = tallyVotes({ upVotes: 13, downVotes: 2 });
+        expect(result).toEqual(11)
+    })
+    it("a function that takes in an object that contains up votes and down votes and returns the end tally for votes2", () => {
+        const result = tallyVotes({ upVotes: 2, downVotes: 33 });
+        expect(result).toEqual(-31);
+      })
+})
 // b) Create the function that makes the test pass.
-
-// Pseudo code:
+const tallyVotes = (votes) => {
+    return votes.upVotes - votes.downVotes
+}
+// Pseudo code: 
+// create a function that uses object as argument
+// get difference from upVotes to downVotes to get tally
+// return calculated tally
--- ruby.rb
+++ ruby.rb
@@ -7,4 +7,34 @@
 # Add comments to your code explaining your approach
 
 class Bike
+    attr_accessor :model, :wheels, :current_speed
+  
+    # initialize Bike Class with model object
+    # creating getter and setter
+    def initialize(model)
+      @model = model
+      @wheels = 2
+      @current_speed = 0
+    end
+  
+    # method to change the number of wheels
+    def wheels=(new_wheels)
+      @wheels = new_wheels
+    end
+  
+    # method to increase speed
+    def pedal_faster(amount)
+      @current_speed += amount
+    end
+  
+    # method to decrease speed
+    # brake method so that speed can't go negative
+    def brake(amount)
+      @current_speed = [@current_speed - amount, 0].max
+    end
+  
+    # method to bike info that includes all previous methods
+    def bike_info
+      "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
 end
